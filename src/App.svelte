f2bf0d096221e7264f9b21d565b1b76adb953efb
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2019-07-12 17:28:54 +0100

    Add Greeting component

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
-	export let name;
+  import Greeting from "./Greeting.svelte";
+  export let name;
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+    color: green;
+  }
 </style>
 
 <h1>Hello {name}!</h1>
+<Greeting first_name="Guillaume" last_name="Bouffard" />
---
+++ src/Greeting.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let first_name;
+  export let last_name;
+  $: name = first_name + " " + last_name;
+</script>
+
+<style>
+  h2 {
+    color: lightgreen;
+    font-family: "Comic Sans MS";
+    font-size: 2em;
+  }
+</style>
+
+<h2>Hello {name}</h2>
